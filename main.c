dcb4b6a3afd304e6989051a1bbffdd5492d46972
Oskari Heinonen <oskariheinonen22@gmail.com>
2024-09-24 11:53:31 +0300

    implement cli

--- main.c
+++ main.c
@@ -1,7 +1,29 @@
 #include <signal.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-int main() {
+#define HELP_STR "Usage: pingpong [<options>]\n\n    --copies, -c        Amount of child process copies to be created\n"
+
+
+int main(int argc, char** argv) {
     
+    if (argc < 2) {
+        printf("No parameters given.\n");
+        printf(HELP_STR);
+        return 1;
+    }
+    else if (!strcmp(argv[1], "-c") || !strcmp(argv[1], "--copies")) {
+        if (argc < 3) {
+            printf("Give the amout of child processes to spawn\n");
+            printf(HELP_STR);
+            return 1;
+        }
+    }
+
+
+    //pid_t pid = fork();
+
     return 0;
 }
